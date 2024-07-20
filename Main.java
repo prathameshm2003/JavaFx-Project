7374ae6e217b7195a09b486d7694b32edc84ccb2
Prathamesh Mandhare <156640394+prathameshm2003@users.noreply.github.com>
2024-07-20 11:13:21 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.evlocator;
+
+import com.evlocator.initialize.InitializeApp;
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Application.launch(InitializeApp.class, args);
+    }
+}
