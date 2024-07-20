d57b91cdf0333b509608c63051f7c2dbe0a60ba8
Prathamesh Mandhare <156640394+prathameshm2003@users.noreply.github.com>
2024-07-20 13:07:14 +0530

    Add files via upload

---
+++ Projects in Maven/Form Navigation/Main.java
@@ -0,0 +1,11 @@
+package com.c2w;
+
+import com.c2w.controller.FormNavigationApp;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Application.launch(FormNavigationApp.class, args);
+    }
+}
