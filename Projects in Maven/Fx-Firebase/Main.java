c45d7a8a2364a06c490e3b7ad8f736a1284567ff
Prathamesh Mandhare <156640394+prathameshm2003@users.noreply.github.com>
2024-07-21 20:24:59 +0530

    Add files via upload

---
+++ Projects in Maven/Fx-Firebase/Main.java
@@ -0,0 +1,12 @@
+package com.firebase;
+
+import com.firebase.controller.LoginController;
+import javafx.application.Application;
+
+public class Main {
+
+    // Launching the LoginController class
+    public static void main(String[] args) {
+        Application.launch(LoginController.class, args);
+    }
+}
