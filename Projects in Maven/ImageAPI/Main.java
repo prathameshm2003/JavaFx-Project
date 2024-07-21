9c5b11444e1757d11328a8efdccdeaed83715a6e
Prathamesh Mandhare <156640394+prathameshm2003@users.noreply.github.com>
2024-07-21 10:35:07 +0530

    Add files via upload

---
+++ Projects in Maven/ImageAPI/Main.java
@@ -0,0 +1,12 @@
+package com.imgapi;
+
+import javafx.application.Application;
+import com.imgapi.service.APIService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Launching the APIService class
+        Application.launch(APIService.class, args);
+    }
+}
